fcf0ef41df1e3888deb0ce6f7216becdaf136ae3
Anuj kashyap <anujkashyap123000@gmail.com>
2023-05-11 22:53:57 +0530

    update portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,6 @@ const Hero = () => {
         </div>
       </div>
 
-    <ComputersCanvas />
 
     <div className='absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center'>
        <a href='#about'>
